f4496ef22a6a246befcf7cea35ece2691c22208f
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-05-05 11:12:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ connection = cx_Oracle.connect("orcl/SYS:admin@localhost:1521/mydb")
 import chart_studio
 chart_studio.tools.set_credentials_file(username='EvelinaBodnar', api_key='B8KF4tdlXXi5cYNGTXUx')
 
-cur = connection.cursor()
+curs = connection.cursor()
 
-cur.execute('''
+curs.execute('''
 SELECT company, COUNT(bar_id) AS count
-    FROM Chocolate
+    FROM Tables
 GROUP BY company
 ORDER BY count DESC
 ''')
-rows = cur.fetchall()
+rows = curs.fetchall()
 x = []
 y = []
 for row in rows:
@@ -26,15 +26,15 @@ fig = go.Figure(data=bar)
 bar_url=py.plot(fig,filename='Company and quantity of chocolates-3', auto_open=False)
 
 
-cur.execute('''
+curs.execute('''
 SELECT company, ROUND(COUNT(bar_name)*100/t.count, 2) AS persent
-FROM Chocolate,
+FROM Tables,
     (SELECT COUNT(bar_name) AS count
-FROM Chocolate)t   
+FROM Tables)t   
 GROUP BY  company,
      t.count
 ''')
-rows = cur.fetchall()
+rows = curs.fetchall()
 x = []
 y = []
 for row in rows:
@@ -45,14 +45,13 @@ pie = go.Figure(data=[go.Pie(labels=y, values=x )])
 pie_url=py.plot(pie, filename='percentage of chocolate company-3',auto_open=False)
 
 
-cur.execute(''' SELECT Bean.bean_type,
+curs.execute(''' SELECT bean_type,
     COUNT(Chocolate.bar_id) AS count
-    FROM Chocolate 
-    INNER JOIN Bean ON Chocolate.bean_type = Bean.bean_type
-GROUP BY Bean.bean_type
+    FROM Tables
+GROUP BY bean_type
 ORDER BY count DESC
 ''')
-rows = cur.fetchall()
+rows = curs.fetchall()
 x = []
 y = []
 for row in rows:
@@ -66,28 +65,28 @@ import re
 import chart_studio.dashboard_objs as dashboard
 
 board = dashboard.Dashboard()
-scatter_beans= fileId(scatter_url)
-pie_choc= fileId(pie_url)
-bar_rating= fileId(bar_url)
+scatter_beans= file(scatter_url)
+pie_choc= file(pie_url)
+bar_rating= file(bar_url)
 
 box_1 = {
     'type': 'box',
     'boxType': 'plot',
-    'fileId':bar_rating,
+    'file':bar_rating,
     'title': 'Company and quantity of chocolatess'
 }
 
 box_2 = {
     'type': 'box',
     'boxType': 'plot',
-    'fileId':pie_choc,
+    'file':pie_choc,
     'title': 'percentage of chocolate company  '
 }
 
 box_3 = {
     'type': 'box',
     'boxType': 'plot',
-    'fileId':scatter_beans ,
+    'file':scatter_beans ,
     'title': 'dependence on the type of beans on the percentage of chocolate'
 }
 
@@ -95,7 +94,7 @@ board.insert(box_1)
 board.insert(box_2, 'below', 1)
 board.insert(box_3, 'left', 2)
 
-py.dashboard.upload(mboard, 'Dashboard.3')
+py.dashboard.upload(board, 'Dashboard.3')
 
 connection.commit()
 cur.close()
