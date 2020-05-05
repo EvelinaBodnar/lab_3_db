cc4665e4d3886d0d14aaa875dc2629bcc6bd273e
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-05-05 11:19:33 +0300

    Update export.py

--- export.py
+++ export.py
@@ -1,16 +1,26 @@
 import csv
 import cx_Oracle
-connection = cx_Oracle.connect("orcl/SYS:admin@localhost:1521/mydb")
+
+
+try:
+ connection = cx_Oracle.connect("orcl/SYS:admin@localhost:1521/mydb")
+except cx_Oracle.DatabaseError as exception:
+    print('Not working database%s\n', database)
+    print(exception)
+    exit(1)
+    
+    
 cur = connection.cursor()
 
-tables = ['Chocolate', 'Company', 'Bean']
-for table in tables:
-    with open(table + '.csv', 'w', newline = '') as csvfile:
+tabl = ['Chocolate', 'Company', 'Bean']
+for table in tabl:
+    with open(table + '.csv', 'w', newline = '') as file:
         cur.execute('SELECT * FROM ' + table)
-        rows = cursor.fetchone()
-        writeCSV = csv.writer(csvfile, delimiter=',')
+        rows = cur.fetchone()
+        wrCSV = csv.writer(file, delimiter=',')
         while rows:
-            writeCSV.writerow(rows)
+            wCSV.writerow(rows)
             rows = cur.fetchone()
+
 cur.close()
 connection.close()
