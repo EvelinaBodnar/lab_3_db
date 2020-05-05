c15ea507c447968ed8e30283ef2dceef9f2db3a0
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-05-05 11:26:45 +0300

    Update kaggle_import.py

--- kaggle_import.py
+++ kaggle_import.py
@@ -1,23 +1,28 @@
 import csv
 import cx_Oracle
+try:
+	conn = cx_Oracle.connect("orcl/SYS:admin@localhost:1521/mydb")
+except cx_Oracle.DatabaseError as exception:
+    print('Not working database %s\n', database)
+    print(exception)
+    exit(1)
 
-connection = cx_Oracle.connect("orcl/SYS:admin@localhost:1521/mydb")
-cur = connection.cursor()
+cur = conn.cursor()
 
-tables = ['Chocolate', 'Company', 'Beans']
-for table in tables:
+tabl = ['Chocolate', 'Company', 'Beans']
+for table in tabl:
     cur.execute("delete " +table)
-csv_file = open('flavors_of_cacao.csv', encoding='utf-8')
-data = csv.reader(csv_file, delimiter=',')
+file = open('flavors_of_cacao.csv', encoding='utf-8')
+rows = csv.reader(file, delimiter=',')
 
 company_un = []
 beens_un = []
-id = 1
-next(data)
-for row in data:
+bar_id = 1
+next(rows)
+for row in rows:
     bar_name = row[1].strip()
-    company = row[2].strip()
-    bean_type = row[3].strip()
+    company = row[0].strip()
+    bean_type = row[-1].strip()
     cocoa_perc = row[4].strip()
    if company not in company_un:
         company_un.append(company)
@@ -28,10 +33,10 @@ for row in data:
         cur.execute("INSERT INTO Bean(bean_type) VALUES(:bean_type)", bean_type=bean_type)
 
     cur.execute("INSERT INTO Chocolate(bar_id, bar_name, company, year) VALUES(:bar_id, :bar_name, :company, :bean_type, :cocoa_perc)",
-		bar_id = id, bar_name = bar_name, company = company, bean_type = bean_type, cocoa_perc = cocoa_perc)
-    id += 1
+		bar_id = bar_id, bar_name = bar_name, company = company, bean_type = bean_type, cocoa_perc = cocoa_perc)
+    bar_id += 1
 
-connection.commit()
+conn.commit()
 cur.close()
-connection.close()
-csv_file.close()
+conn.close()
+file.close()
