d96d38e458806ad3202e6f8d272e55fb9c28fbf6
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-05-06 11:48:45 +0300

    Update view.sql

--- view.sql
+++ view.sql
@@ -1,9 +1,11 @@
 CREATE OR REPLACE VIEW Tables AS
     SELECT
         Chocolate.bar_id,
+        Chocolate.bar_name,
         Company.company,
         Bean.bean_type,
-        Chocolate.rating
+        Chocolate.cocoa_perc
+
     FROM
         Chocolate
         JOIN Company ON Chocolate.company = Company.company
