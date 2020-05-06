54f7eb0a3d8aca98d13a9060c32e6c145d4fc0e1
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-05-06 11:36:31 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -1,6 +1,7 @@
 DECLARE
-    TYPE CHARS IS VARRAY(15) OF Chocolate.company%TYPE;
+    TYPE NUMBERS IS VARRAY(15) OF Chocolate.bar_id%TYPE;
     TYPE CHARS IS VARRAY(15) OF Chocolate.bar_name%TYPE;
+    TYPE FLOATS IS VARRAY(15) OF Chocolate.cocoa_perc%TYPE;
 
 BEGIN
     bar_id := NUMBERS(12,16,30,32,40,44,46);
@@ -8,11 +9,10 @@ BEGIN
     company := CHARS('Soma','Bonnat','Bonnat','Fresco','Pralus','A.Morin','Fresco');
     bean_type := CHARS('OER','Criollo','Forastero','Criollo','Trinitario','Trinitario','OER');
     cocoa_perc := FLOATS(80,35,56,64,72,30,43);
-    rating := FLOATS(1.2,4.5,3.8,2.3,3.2,4.4,5.0);
-    
-    
-    FOR i IN 1 .. bar_id.count LOOP
-        INSERT INTO Chocolate (bar_id,bar_name,company,bean_type,cocoa_perc,rating) VALUES (bar_id(i), bar_name(i), company(i), bean_type(i), cocoa_perc(i), rating(i));
+
+    FOR i IN 1 .. bar_id.count
+    LOOP
+        INSERT INTO Chocolate (bar_id,bar_name,company,bean_type,cocoa_perc) VALUES (bar_id(i), bar_name(i), company(i), bean_type(i), cocoa_perc(i));
         COMMIT;
     END LOOP;
 END;
