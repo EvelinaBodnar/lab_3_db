ed7867d169c2412f4c10a9465646503fd36298cb
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-05-25 09:30:26 +0300

    Update loop.sql

--- loop.sql
+++ loop.sql
@@ -1,18 +1,14 @@
-DECLARE
-    TYPE NUMBERS IS VARRAY(15) OF Chocolate.bar_id%TYPE;
-    TYPE CHARS IS VARRAY(15) OF Chocolate.bar_name%TYPE;
-    TYPE FLOATS IS VARRAY(15) OF Chocolate.cocoa_perc%TYPE;
-
+DECLARE 
+    items_count INT NOT NULL DEFAULT 1; 
 BEGIN
-    bar_id := NUMBERS(12,16,30,32,40,44,46);
-    bar_name := CHARS('Madaga','Kpim','Mad','Quil','Akat','Dominic','Ecuado');
-    company := CHARS('Soma','Bonnat','Bonnat','Fresco','Pralus','A.Morin','Fresco');
-    bean_type := CHARS('OER','Criollo','Forastero','Criollo','Trinitario','Trinitario','OER');
-    cocoa_perc := FLOATS(80,35,56,64,72,30,43);
+    FOR i IN 1..items_count LOOP
+        INSERT INTO Chocolate (bar_id, bar_name, company, bean_type, cocoa_perc)
+            VALUES ('11', 'Madaga', 'Soma', 'Criollo', '80'); 
 
-    FOR i IN 1 .. bar_id.count
-    LOOP
-        INSERT INTO Chocolate (bar_id,bar_name,company,bean_type,cocoa_perc) VALUES (bar_id(i), bar_name(i), company(i), bean_type(i), cocoa_perc(i));
-        COMMIT;
+        INSERT INTO Chocolate (bar_id, bar_name, company, bean_type, cocoa_perc)
+            VALUES ('12', 'Kpim', 'Bonnat', 'Criollo', '45');
+
+        INSERT INTO Chocolate (bar_id, bar_name, company, bean_type, cocoa_perc)
+            VALUES ('13', 'Akat', 'Fresco', 'Trinitario', '75'); 
     END LOOP;
 END;
