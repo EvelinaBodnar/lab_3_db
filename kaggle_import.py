db9c44f5a40f775c7f5be03f51300a9233455049
EvelinaBodnar <43322909+EvelinaBodnar@users.noreply.github.com>
2020-05-25 13:54:24 +0300

    Update kaggle_import.py

--- kaggle_import.py
+++ kaggle_import.py
@@ -1,42 +1,47 @@
 import csv
 import cx_Oracle
+
+username = 'viktoriya'
+password = 'viktoriya'
+database = 'localhost:1521/xe'
+connection = cx_Oracle.connect(username,password, database)
+cursor = connection.cursor()
+
+csvfile = open('flavors_of_cacao.csv', errors='ignore', encoding='utf-8')
+readCSV = csv.reader(csvfile, delimiter=',')
+bar_id = 0
+
+company_unique = []
+
+
+tables = ['Chocolate', 'Company']
+for table in tables:
+    cursor.execute("DELETE FROM " + table)
+
 try:
-	conn = cx_Oracle.connect("orcl/SYS:admin@localhost:1521/mydb")
-except cx_Oracle.DatabaseError as exception:
-    print('Not working database %s\n', database)
-    print(exception)
-    exit(1)
+    for row in readCSV:
+        bar_name = row[1].strip()
+        company = row[0].strip()
+        bean_type = row[-1].strip()
+        cocoa_perc = row[4].strip()
 
-cur = conn.cursor()
+        if company not in company_unique:
+            company_unique.append(company)
+            query = '''INSERT INTO Company(company) VALUES(:company)'''
+            cursor.execute(query, company=company)
 
-tabl = ['Chocolate', 'Company', 'Beans']
-for table in tabl:
-    cur.execute("delete " +table)
-file = open('flavors_of_cacao.csv', encoding='utf-8')
-rows = csv.reader(file, delimiter=',')
 
-company_un = []
-beens_un = []
-bar_id = 1
-next(rows)
-for row in rows:
-    bar_name = row[1].strip()
-    company = row[0].strip()
-    bean_type = row[-1].strip()
-    cocoa_perc = row[4].strip()
-   if company not in company_un:
-        company_un.append(company)
-        cur.execute("INSERT INTO Company(company) VALUES(:company)", company=company)
+        query = '''
+        INSERT INTO Chocolate(bar_id, bar_name, company, bean_type, cocoa_perc) VALUES(:bar_id, :bar_name, :company, :bean_type, :cocoa_perc)'''
+        cursor.execute(query, bar_id = bar_id, bar_name = bar_name, company = company, bean_type = bean_type, cocoa_perc = cocoa_perc)
 
-    if bean_type not in beens_un:
-        beens_un.append(bean_type)
-        cur.execute("INSERT INTO Bean(bean_type) VALUES(:bean_type)", bean_type=bean_type)
+        bar_id += 1
 
-    cur.execute("INSERT INTO Chocolate(bar_id, bar_name, company, year) VALUES(:bar_id, :bar_name, :company, :bean_type, :cocoa_perc)",
-		bar_id = bar_id, bar_name = bar_name, company = company, bean_type = bean_type, cocoa_perc = cocoa_perc)
-    bar_id += 1
+except:
+    print(f'Error of the row {bar_id}')
+    raise
 
-conn.commit()
-cur.close()
-conn.close()
-file.close()
+csvfile.close()
+connection.commit()
+cursor.close()
+connection.close()
